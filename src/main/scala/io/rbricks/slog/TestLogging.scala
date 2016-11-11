9a1259924b6eb64722f2153f2f3b6cdf2807ebe6
Andrea Lattuada <andrea@lattuada.me>
2016-11-11 19:07:58 +0100

    Logging backend for tests

---
+++ src/main/scala/io/rbricks/slog/TestLogging.scala
@@ -0,0 +1,39 @@
+package io.rbricks.slog
+
+import org.slf4j.helpers.MessageFormatter
+
+trait TestLogging
+
+private[slog] class TestLoggingImpl(private val level: Level) extends TestLogging {
+
+  private val transports = Seq(new transport.Console(colorized = true))
+
+  private def writeToTransports(name: String, logMessage: LogMessage) = transports.foreach(_.write(name, logMessage))
+      
+  // private val loggingLogger = Logger("io.rbricks.slog.TestLogging", transports, { case _ => true })
+
+  private val loggerNames = scala.collection.mutable.HashSet[String]()
+
+  @inline
+  private[this] def loggersEnabled(name: String): Map[Level, Boolean] = {
+    Map[Level, Boolean](
+      Level.Debug -> (level.value >= Level.Debug.value),
+      Level.Info  -> (level.value >= Level.Info.value ),
+      Level.Warn  -> (level.value >= Level.Warn.value ),
+      Level.Error -> (level.value >= Level.Error.value)
+    )
+  }
+
+  org.slf4j.impl.SimpleLoggerFactory.setLoggerFactoryInterface(new org.slf4j.impl.LoggerFactoryInterface {
+    def getNewLogger(name: String): org.slf4j.Logger = {
+      val enabled = loggersEnabled(name)
+      new Logger(name, enabled, writeToTransports)
+    }
+  })
+
+}
+
+
+object TestLogging {
+  def apply(level: Level = Level.Info): TestLogging = new TestLoggingImpl(level)
+}
